2f1e6370ea2aadce0dff68c708a1ff5b84d475a1
Adrien Lefèvre <adlef01@gmail.com>
2020-09-03 17:55:10 +0200

    adding inclusion to solve issue in loading pickle model (DisasterMessages Project)

--- myapp.py
+++ myapp.py
@@ -1,2 +1,5 @@
+from wrangling_scripts.DisasterMessages.train_classifier import tokenize
+
 from myapp import app
-#app.run(host='0.0.0.0', port=3001, debug=True)
+
+app.run(host='0.0.0.0', port=3001, debug=True)
